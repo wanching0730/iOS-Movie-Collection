13941a47d7bea5e67840f2b94a2609e3f94a1542
wanching_97 <wanching_97@hotmail.com>
2018-08-08 02:08:14 +0800

    added rating dropdown

--- MovieCollection/AddViewController.swift
+++ MovieCollection/AddViewController.swift
@@ -12,27 +12,62 @@ import DropDown
 class AddViewController: UIViewController {
 
     @IBOutlet weak var categoryButton: UIButton!
+    @IBOutlet weak var ratingButton: UIButton!
     
-    let dropDown = DropDown()
+    let categoryDropDown = DropDown()
+    let ratingDropDown = DropDown()
+    
+    lazy var dropDowns: [DropDown] = {
+        return [
+            self.categoryDropDown,
+            self.ratingDropDown
+        ]
+    }()
     
     override func viewDidLoad() {
-        dropDown.anchorView = categoryButton
-        dropDown.dataSource = ["Action", "Comedy", "Horror", "Romance"]
-        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
-                self.categoryButton.setTitle(item, for: .normal)
-                print("Selected item: \(item) at index: \(index)")
-
-        }
-        dropDown.width = 120
-        dropDown.direction = .any
-        dropDown.dismissMode = .onTap
-        dropDown.bottomOffset = CGPoint(x: 0, y: categoryButton.bounds.height)
+        setupDropDown()
+        dropDowns.forEach { $0.width = 120 }
+        dropDowns.forEach { $0.dismissMode = .onTap }
+        dropDowns.forEach { $0.direction = .bottom }
         
     }
     
     
     @IBAction func selectCategoryPressed(_ sender: UIButton) {
-        dropDown.show()
+        categoryDropDown.show()
+    }
+    
+    @IBAction func selectRatingPressed(_ sender: UIButton) {
+        ratingDropDown.show()
+    }
+    
+    func setupDropDown() {
+        setupCategoryDropDown()
+        setupRatingDropDown()
+    }
+    
+    func setupCategoryDropDown() {
+        categoryDropDown.anchorView = categoryButton
+        categoryDropDown.bottomOffset = CGPoint(x: 0, y: categoryButton.bounds.height)
+        categoryDropDown.dataSource = ["Action", "Comedy", "Horror", "Romance"]
+        categoryDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            self.categoryButton.setTitle(item, for: .normal)
+            print("Selected category: \(item) at index: \(index)")
+            
+        }
+
+    }
+    
+    func setupRatingDropDown() {
+        ratingDropDown.anchorView = ratingButton
+        ratingDropDown.bottomOffset = CGPoint(x: 0, y: ratingButton.bounds.height)
+        ratingDropDown.dataSource = ["1", "2", "3", "4", "5"]
+        ratingDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            self.ratingButton.setTitle(item, for: .normal)
+            print("Selected rating: \(item) at index: \(index)")
+            
+        }
+
     }
 }
 
