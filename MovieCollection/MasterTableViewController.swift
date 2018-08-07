6fb93f35c25fdc16e22e0634a3d7141ee0ac371e
wanching_97 <wanching_97@hotmail.com>
2018-08-07 23:42:22 +0800

    attach class to all view controller

--- MovieCollection/MasterTableViewController.swift
+++ MovieCollection/MasterTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MasterTableViewController: UIViewController {
+class MasterTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
