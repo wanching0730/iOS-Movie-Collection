f370013306b62061d472d9768bf3ba5c5f547553
wanching_97 <wanching_97@hotmail.com>
2018-08-08 01:50:05 +0800

    added category dropdown

--- MovieCollection/AddViewController.swift
+++ MovieCollection/AddViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import DropDown
 
 class AddViewController: UIViewController {
 
+    @IBOutlet weak var categoryButton: UIButton!
+    
+    let dropDown = DropDown()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        dropDown.anchorView = categoryButton
+        dropDown.dataSource = ["Action", "Comedy", "Horror", "Romance"]
+        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+                self.categoryButton.setTitle(item, for: .normal)
+                print("Selected item: \(item) at index: \(index)")
+
+        }
+        dropDown.width = 120
+        dropDown.direction = .any
+        dropDown.dismissMode = .onTap
+        dropDown.bottomOffset = CGPoint(x: 0, y: categoryButton.bounds.height)
+        
+    }
+    
+    
+    @IBAction func selectCategoryPressed(_ sender: UIButton) {
+        dropDown.show()
     }
 }
 
